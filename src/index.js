d2c3cc58e2e02ec0d74cdd8605c081090d37797f
noviantia <noviantialiasih@ruangguru.com>
2022-09-22 09:31:38 +0700

    Add class Title

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as ReactDOMClient from 'react-dom/client';
 import '/app.css'
 
 const linkStyle = {
@@ -7,15 +8,31 @@ const linkStyle = {
     color: '#42a1f5'
 };
 
-const element = (
-    <>
-    <div>
-        <img src="https://imgix3.ruangguru.com/assets/user_uploads/pp/pp_iqo2rc_8285.png" width="150"/>
-        <h1>Hello React's World</h1>
-        <a href="https://reactjs.org/" target="_blank" style={linkStyle}>Learn React</a>
-    </div>
-    </>
-);
+class Title extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        text: 'React'
+      };
+    };
+  
+    handleChange(event) {
+      this.setState({
+        text: event.target.value
+      });
+    };
+  
+    render() {
+      return (
+        <div>
+          <img src="https://imgix3.ruangguru.com/assets/user_uploads/pp/pp_iqo2rc_8285.png" width="150" height="106"/>
+          <input type="text" onKeyUp={this.handleChange.bind(this)} />
+          <h1>Hello {this.state.text}'s World</h1>
+          <a href="https://reactjs.org/" target="_blank" style={linkStyle}>Learn React</a>
+        </div>
+      );
+    }
+}
 
-const root = document.getElementById('app');
-ReactDOM.render(element, root);
+const root = ReactDOMClient.createRoot(document.getElementById('app'));
+root.render(<Title />);
