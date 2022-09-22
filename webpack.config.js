a2e4af8379cd2dc5839061acb931647522d36250
noviantia <noviantialiasih@ruangguru.com>
2022-09-22 09:08:25 +0700

    Update body styling

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,10 @@ module.exports = {
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
                 use: ['babel-loader']
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
             }
         ]
     } 
